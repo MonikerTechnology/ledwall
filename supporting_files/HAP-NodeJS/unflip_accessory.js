0b5d20726b2ea871b139c65ce721b4c0e6558455
Cody McComber <codymccomber@gmail.com>
2018-05-12 20:07:21 -0400

    unflip

--- supporting_files/HAP-NodeJS/unflip_accessory.js
+++ supporting_files/HAP-NodeJS/unflip_accessory.js
@@ -3,6 +3,19 @@ var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
+////////////////CHANGE THESE SETTINGS TO MATCH YOUR SETUP BEFORE RUNNING!!!!!!!!!!!!!//////////////////////////
+////////////////CHANGE THESE SETTINGS TO MATCH YOUR SETUP BEFORE RUNNING!!!!!!!!!!!!!//////////////////////////
+var name = "HSVdata";                                       //Name to Show to IOS
+var UUID = "hap-nodejs:accessories:HSVdata";     //Change the RGBLight to something unique for each light - this should be unique for each node on your system
+var USERNAME = "00:00:00:2C:5D:C1";              //This must also be unique for each node - make sure you change it!
+
+var MQTT_IP = '127.0.0.1'
+var lightTopic = '/LEDwall'
+////////////////CHANGE THESE SETTINGS TO MATCH YOUR SETUP BEFORE RUNNING!!!!!!!!!!!!!//////////////////////////
+////////////////CHANGE THESE SETTINGS TO MATCH YOUR SETUP BEFORE RUNNING!!!!!!!!!!!!!//////////////////////////
+
+
+
 var state = false;
 
 var accUUID = uuid.generate('hap-nodejs:accessories:switch');
