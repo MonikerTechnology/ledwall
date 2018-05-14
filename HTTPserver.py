49f5a78e2b76ce8fe6e67afc04b5fcfc9cc0113d
Cody McComber <codymccomber@gmail.com>
2018-05-13 20:30:29 -0400

    inc

--- HTTPserver.py
+++ HTTPserver.py
@@ -42,8 +42,8 @@ class S(BaseHTTPRequestHandler):
         global post_data
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+        #        str(self.path), str(self.headers), post_data.decode('utf-8'))
         #print
 
         #message = post_data.decode('utf-8')
