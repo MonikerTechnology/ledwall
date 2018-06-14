4657527c6d87bfe877db7c26f07753d613d70cba
Cody McComber <codymccomber@gmail.com>
2018-06-13 21:34:49 -0400

    sdf

--- audio.py
+++ audio.py
@@ -118,7 +118,7 @@ def getPositionVolume():
     maxVolumeList.append(maxVolumeLoop)
     if len(maxVolumeList) > 10:
         maxVolumeScale = calcVolume(maxVolumeList)
-        print("Scale: ",maxVolumeScale, " maxLoop: " max(maxVolumeList))
+        print("Scale: ",maxVolumeScale, " maxLoop: " ,max(maxVolumeList))
         maxVolumeScale = max(maxVolumeList)
         maxVolumeList[:] = []
 
