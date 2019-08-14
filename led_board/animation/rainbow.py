fef3a051a92e2be157a433e2d8caf3c70a35e46d
codymcco <codymccomber@gmail.com>
2019-08-13 21:51:40 -0700

    Updates

--- led_board/animation/rainbow.py
+++ led_board/animation/rainbow.py
@@ -66,4 +66,4 @@ def rainbow(t, coord, ii, n_pixels, random_values):
     # only do this on live leds, not in the simulator
     r, g, b = color_utils.gamma((r, g, b), 2.2)
 
-    return (r, g, b)
+    return (r * 255, g * 255, b * 100)
