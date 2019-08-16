8b2f77ffdfcb43534de43b8ca0b172f9db6d0b98
codymcco <codymccomber@gmail.com>
2019-08-16 12:00:42 -0700

    Updates

--- ledwall/settings.py
+++ ledwall/settings.py
@@ -13,11 +13,12 @@ class Settings:
     @classmethod
     def update_values(cls, **kwargs):
         for key, value in kwargs.items():
-            logging.debug("%s == %s" % (key, value))
 
             if isinstance(value, list):
+                logging.debug("%s == %s" % (key, value[0]))
                 cls.key = value[0]
             else:
+                logging.debug("%s == %s" % (key, value))
                 cls.key = value
 
 
