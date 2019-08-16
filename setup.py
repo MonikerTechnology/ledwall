ddf36fa1bfd472245e5c7a0cf47959c2f9b6c4f1
codymcco <codymccomber@gmail.com>
2019-08-16 14:00:51 -0700

    Copied files with setup.py

--- setup.py
+++ setup.py
@@ -144,10 +144,10 @@ setup(
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[('supporting_files',
-                 ['supporting_files/fcserver-osx',
-                  'supporting_files/fcserver-rpi',
-                  'supporting_files/fcserver_config.json',
-                  'supporting_files/fcserver-ledwall15x9.json'])],  # Optional
+                 ['ledwall/supporting_files/fcserver-osx',
+                  'ledwall/supporting_files/fcserver-rpi',
+                  'ledwall/supporting_files/fcserver_config.json',
+                  'ledwall/supporting_files/fcserver-ledwall15x9.json'])],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
