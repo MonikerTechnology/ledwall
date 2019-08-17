9eae059aba104b1c139f7826f652a79e75e87cf8
codymcco <codymccomber@gmail.com>
2019-08-16 18:50:54 -0700

    Making audio

--- ledwall/animation/a_audio_bars.py
+++ ledwall/animation/a_audio_bars.py
@@ -33,12 +33,12 @@ def audio_bars(t, random_values, audio_o: AudioProcessor, coordinates):
 
         # Highest pixel and diff color (but low y value)
         audio_channel_volume = audio_o.data_dict[coord[x]]['max_volume']
-        if audio_channel_volume > vol_levels * coord[y]:
+        if audio_channel_volume < vol_levels * coord[y]:
             this_pixel = (250, 250, 250)
 
-        #
-        if vol_levels * (coord[2] + 1) > audio_o.data_dict[coord[0]]['falling_max'] > vol_levels * coord[2]:
-            this_pixel = (150, 150, 150)
+        # Rest of the bar
+        if vol_levels * (coord[y] + 1) < audio_o.data_dict[coord[0]]['falling_max'] < vol_levels * coord[2]:
+            this_pixel = (150, 15, 150)
 
         pixels.append(this_pixel)
     return pixels  # Returns a list of pixels
