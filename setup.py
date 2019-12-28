a2bd628abf2a0d61851565561af43d10b84b3e86
codymcco <codymccomber@gmail.com>
2019-12-28 11:39:51 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -46,44 +46,6 @@ setup(
     # https://packaging.python.org/specifications/core-metadata/#summary
     description='Controlls LED wall',  # Optional
 
-    # This is an optional longer description of your project that represents
-    # the body of text which users will see when they visit PyPI.
-    #
-    # Often, this is the same as your README, so you can just read it in from
-    # that file directly (as we have already done above)
-    #
-    # This field corresponds to the "Description" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-optional
-    long_description=long_description,  # Optional
-
-    # Denotes that our long_description is in Markdown; valid values are
-    # text/plain, text/x-rst, and text/markdown
-    #
-    # Optional if long_description is written in reStructuredText (rst) but
-    # required for plain-text or Markdown; if unspecified, "applications should
-    # attempt to render [the long_description] as text/x-rst; charset=UTF-8 and
-    # fall back to text/plain if it is not valid rst" (see link below)
-    #
-    # This field corresponds to the "Description-Content-Type" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
-    # This should be a valid link to your project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/pypa/sampleproject',  # Optional
-
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='The Python Packaging Authority',  # Optional
-
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='pypa-dev@googlegroups.com',  # Optional
-
-
-
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?
     #
