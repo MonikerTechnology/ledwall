da340e3937072747b8e827a57c736ea50f1615f2
CodyMcC <cody@mccomber.me>
2021-10-14 18:25:20 -0400

    threading

--- ledwall/__main__.py
+++ ledwall/__main__.py
@@ -268,6 +268,7 @@ def main():
                 print(f"{Settings.rgb_last} - {Settings.rgb}")
                 Settings.rgb_last = color_utils.fade_pixel(Settings.rgb_last, Settings.rgb)
                 pixels.fill(Settings.rgb_last)
+                pixels.show()
                 # if Settings.mode == "rainbow":
                 #     pass
 
