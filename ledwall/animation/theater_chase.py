6f1d4c03afd7028194ca8a615efac505d3a62334
CodyMcC <cody@mccomber.me>
2021-10-15 09:34:54 -0400

    modes

--- ledwall/animation/theater_chase.py
+++ ledwall/animation/theater_chase.py
@@ -1,10 +1,10 @@
 from ledwall.color_utils import order_to_array, wheel
-from time import sleep
+from time import sleep, perf_counter
 
 
-def theaterChaseRainbow(pixels, counter, wait_ms=50):
+def theaterChaseRainbow(pixels, counter, wait_ms=5):
     """Rainbow movie theater light style chaser animation."""
-
+    start = perf_counter()
     for q in range(3):
         for i in range(0, pixels.n, 3):
             pixels[order_to_array(i + q)] = wheel((i + counter) % 255)
