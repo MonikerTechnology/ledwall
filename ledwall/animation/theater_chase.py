a307b87dbcb7012cc4b1bf5c52554898f818942b
CodyMcC <cody@mccomber.me>
2021-10-15 09:49:52 -0400

    modes

--- ledwall/animation/theater_chase.py
+++ ledwall/animation/theater_chase.py
@@ -8,7 +8,15 @@ def theaterChaseRainbow(pixels, counter_a, counter_b, wait_ms=5):
     # for counter_b in range(3):
     for i in range(0, pixels.n, 3):
         pixels[order_to_array(i + counter_b)] = wheel((i + counter_a) % 255)
+    end = perf_counter()
+    print(f"Picking values took: {end-start}s")
+    start = perf_counter()
+
     pixels.show()
+    end = perf_counter()
+    print(f"show() took: {end-start}s")
+
+
     # sleep(wait_ms / 1000.0)
     for i in range(0, pixels.n, 3):
         pixels[order_to_array(i + counter_b)] = (0, 0, 0)
