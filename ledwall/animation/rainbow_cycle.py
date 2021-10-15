f52b33e798cddaf8e70a4f2f3e2b49ffa11b1e64
CodyMcC <cody@mccomber.me>
2021-10-14 21:14:42 -0400

    trouble

--- ledwall/animation/rainbow_cycle.py
+++ ledwall/animation/rainbow_cycle.py
@@ -1,8 +1,15 @@
 from ledwall.color_utils import wheel
+from time import perf_counter
 
 
 def rainbow_cycle(pixels, index, num_pixels):
     for i in range(num_pixels):
+        start = perf_counter()
         pixel_index = (i * 256 // num_pixels) + index
+        end = perf_counter()
+        print(f"pixel index took {end - start}")
+        start = perf_counter()
         pixels[i] = wheel(pixel_index & 255)
+        end = perf_counter()
+        print(f"pixels[i] took {end - start}")
 
