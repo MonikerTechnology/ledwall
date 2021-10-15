3155f439e163d640da2311d588e6a14bd9a7af87
CodyMcC <cody@mccomber.me>
2021-10-15 09:36:36 -0400

    modes

--- ledwall/animation/theater_chase.py
+++ ledwall/animation/theater_chase.py
@@ -9,7 +9,7 @@ def theaterChaseRainbow(pixels, counter, wait_ms=5):
         for i in range(0, pixels.n, 3):
             pixels[order_to_array(i + q)] = wheel((i + counter) % 255)
         pixels.show()
-        sleep(wait_ms / 1000.0)
-        for i in range(0, pixels.n, 3):
-            pixels[order_to_array(i + q)] = (0, 0, 0)
-            pixels.show()
+        # sleep(wait_ms / 1000.0)
+        # for i in range(0, pixels.n, 3):
+        #     pixels[order_to_array(i + q)] = (0, 0, 0)
+        #     pixels.show()
