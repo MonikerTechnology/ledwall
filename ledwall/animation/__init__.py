e2047e633c5bf0da01493b7faf70aff730b73c24
CodyMcC <cody@mccomber.me>
2021-10-28 19:59:41 -0400

    modes

--- ledwall/animation/__init__.py
+++ ledwall/animation/__init__.py
@@ -5,9 +5,9 @@ logging.debug('\n\nran __init__ in the animation folder\n\n')
 
 # from .a_audio_bars import audio_bars
 from .rainbow import rainbow
-from .a_start_up import start_up
-from .a_solid import solid
-from .test import test
-from .spatial import spatial
-from .raver import raver
-from .sailor import sailor
+# from .a_start_up import start_up
+# from .a_solid import solid
+# from .test import test
+# from .spatial import spatial
+# from .raver import raver
+# from .sailor import sailor
